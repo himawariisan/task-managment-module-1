855f1bded3ede814d2ea0e06610a754e0c9c4a1b
Sofiia <sofarukomeda@gmail.com>
2026-04-08 15:39:03 +0300

    feat: Add Task.__init__ method

---
+++ task_manager/task.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+class Task:
+    def __init__(self, task_id, description, priority):
+        self.task_id = task_id
+        self.description = description
+        self.priority = priority
+        self.creation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.completed = False

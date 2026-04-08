3ad14d9255745df107e141e8b8219367913c4cde
Sofiia <sofarukomeda@gmail.com>
2026-04-08 15:46:36 +0300

    feat: Add Task.to_dict and Task.from_dict methods

--- task_manager/task.py
+++ task_manager/task.py
@@ -11,3 +11,19 @@ class Task:
     
     def mark_as_completed(self):
         self.completed = True
+
+    def to_dict(self):
+        return {
+            "task_id": self.task_id,
+            "description": self.description,
+            "priority": self.priority,
+            "creation_date": self.creation_date,
+            "completed": self.completed,
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        task = cls(data["task_id"], data["description"], int(data["priority"]))
+        task.creation_date = data["creation_date"]
+        task.completed = data["completed"] == "True" or data["completed"] is True
+        return task

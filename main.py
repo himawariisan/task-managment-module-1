33f659ea1f3e70ffcc1494b6dc395a72747dcc92
Sofiia <sofarukomeda@gmail.com>
2026-04-08 15:50:35 +0300

    feat: Add print menu

---
+++ main.py
@@ -0,0 +1,10 @@
+from task_manager.manager import TaskManager
+ 
+ 
+def print_menu():
+    print("\n=== Task Management System ===")
+    print("1. Add task")
+    print("2. Delete task")
+    print("3. List tasks")
+    print("4. Mark task as completed (and remove)")
+    print("5. Exit")
